b68367a8059112f304e377246fdf61f986a93c90
Thanabodee Saepui <thanabodee.saepui@gmail.com>
2024-11-22 22:48:38 +0700

    feat: Add products on homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thanabodeesaepui.github.io/',
   base: 'makusu-site',
-  integrations: [tailwind()]
+  integrations: [tailwind(), solidJs()]
 });
