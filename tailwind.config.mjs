e8e02f6603a75edc22d6d24743583b01c4cb392e
Thanabodee Saepui <thanabodee.saepui@gmail.com>
2024-11-24 22:53:52 +0700

    feat: homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"primary": "#9dbf27",
-				"secondary": "#22953f",
-				"accent": "",
-				"neutral": "",
-				"base-100": "",
-				"info": "#007cc6",
-				"success": "#00bc5a",
-				"warning": "#aa7900",
-				"error": "#ff668c",
-			}
-		},
-	},
-	plugins: [daisyui],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9dbf27",
+        secondary: "#22953f",
+        accent: "",
+        neutral: "",
+        "base-100": "",
+        info: "#007cc6",
+        success: "#00bc5a",
+        warning: "#aa7900",
+        error: "#ff668c",
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        theme: {
+          "primary": "#9dbf27",
+          "secondary": "#22953f",
+          "accent": "#38bdf8",
+          "neutral": "#9ca3af",
+          "base-100": "#1f2937",
+          "info": "#22d3ee",
+          "success": "#2d9a00",
+          "warning": "#f59e0b",
+          "error": "#e11d48",
+        },
+      },
+    ],
+  },
+  plugins: [daisyui],
+};
