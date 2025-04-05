a5d4ce49d0657b00ad3e95a2bc1282441500aa82
Thanabodee Saepui <thanabodee.saepui@gmail.com>
2025-04-06 00:36:46 +0700

    fix: remove wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://thanabodeesaepui.github.io/',
   // base: 'makusu-site',
   integrations: [tailwind(), solidJs()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
 });
