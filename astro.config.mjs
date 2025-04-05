8b3496c276a9b31d6afec7617993b4d4a82c2e4d
Thanabodee Saepui <thanabodee.saepui@gmail.com>
2025-04-06 00:24:33 +0700

    feat: add cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thanabodeesaepui.github.io/',
-  base: 'makusu-site',
-  integrations: [tailwind(), solidJs()]
+  // site: 'https://thanabodeesaepui.github.io/',
+  // base: 'makusu-site',
+  integrations: [tailwind(), solidJs()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
